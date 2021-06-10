189b5461cd4ba422bc150166ff4828c69697d4d2
mraihan19 <rbfahim.iub13@outlook.com>
2021-06-10 17:28:21 +0600

    :hammer: csrf add

--- crud_app/urls.py
+++ crud_app/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 from graphene_django.views import GraphQLView
 from crud_app.schema import schema
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('graphql/', GraphQLView.as_view(graphiql = True)),
+    # path('graphql/', GraphQLView.as_view(graphiql = True)),
+    path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql = False))),
 ]
